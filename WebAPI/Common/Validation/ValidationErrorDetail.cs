510aa3dd8b702669c04f879503d141c86f544fbf
igorthebarros <1igorhbarros@gmail.com>
2025-04-02 18:06:01 -0300

    :sparkles: Add Validator; Main, Error and Detail

---
+++ WebAPI/Common/Validation/ValidationErrorDetail.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+
+namespace Common.Validation
+{
+    public class ValidationErrorDetail
+    {
+        public string Error { get; set; } = string.Empty;
+        public string Detail { get; set; } = string.Empty;
+
+        public static explicit operator ValidationErrorDetail(ValidationFailure failure)
+        {
+            return new ValidationErrorDetail
+            {
+                Error = failure.ErrorMessage,
+                Detail = failure.ErrorCode
+            };
+        }
+    }
+}
---
+++ WebAPI/Common/Validation/ValidationResultDetail.cs
@@ -0,0 +1,16 @@
+﻿namespace Common.Validation
+{
+    public class ValidationResultDetail
+    {
+        public bool IsValid { get; set; }
+        public IEnumerable<ValidationErrorDetail> Errors { get; set; } = [];
+
+        public ValidationResultDetail() { }
+
+        public ValidationResultDetail(ValidationResultDetail result)
+        {
+            IsValid = result.IsValid;
+            Errors = result.Errors.Select(x => (ValidationErrorDetail)x);
+        }
+    }
+}
