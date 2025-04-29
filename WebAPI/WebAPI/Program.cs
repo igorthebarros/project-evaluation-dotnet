e7e87b6a40ebc8e6038f514d2ab63d0adc57aedd
igorthebarros <1igorhbarros@gmail.com>
2025-04-29 19:51:53 -0300

    :fire: Delete Weatherforecast classes

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -1,23 +1,49 @@
-var builder = WebApplication.CreateBuilder(args);
+using Database;
+using IoC;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+namespace WebApi;
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.MapOpenApi();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Register Custom IoC
+        builder.RegisterDependencies();
+
+        // Infrastructure - ORM Database
+        builder.Services.AddDbContext<DefaultContext>(x =>
+            x.UseNpgsql(
+                builder.Configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly("Database")
+            )
+        );
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "web api v1");
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
