a0f3c1e314f6ba20bdd8520e740ff4a2ab418496
igorthebarros <1igorhbarros@gmail.com>
2025-04-29 19:53:52 -0300

    :sparkles: Add inversion of control implementations

---
+++ WebAPI/IoC/ModuleInitializers/ApplicationModuleInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Application.Services;
+using Domain.Contracts.Services;
+using IoC.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IoC.ModuleInitializers
+{
+    public class ApplicationModuleInitializer : IModuleInitializer
+    {
+        public void Initialize(WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IOrderService, OrderService>();
+        }
+    }
+}
