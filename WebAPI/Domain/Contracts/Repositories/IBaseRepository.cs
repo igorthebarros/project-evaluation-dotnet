f830a8d029991919f92d5681cfbd629fe2213e16
igorthebarros <1igorhbarros@gmail.com>
2025-04-29 19:51:05 -0300

    :adhesive_bandage: Fix IBaseRepository and OrderRepository

--- WebAPI/Domain/Contracts/Repositories/IBaseRepository.cs
+++ WebAPI/Domain/Contracts/Repositories/IBaseRepository.cs
@@ -1,31 +1,32 @@
-﻿namespace Domain.Contracts
+﻿namespace Domain.Contracts.Repositories
 {
-    public interface IBaseService<T> where T : class
+    public interface IBaseRepository<T> where T : class
     {
         /// <summary>
-        /// Create a new entity, handling any business rule before sending the entity to the Repository
+        /// Creates a new entity in the repository
         /// </summary>
         /// <param name="T">The entity to be created</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns>The created entity</returns>
         Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default);
+
         /// <summary>
-        /// Retrieves the entity, handling any business rule before sending the entity to the Repository
+        /// Retrieves a entity in the repository based on it's Guid Id
         /// </summary>
         /// <param name="id">Entity Id on Guid format</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns>The found entity</returns>
-        Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Retrieve all entities, handling any business rule before sending the entity to the Repository
+        /// Retrieve all entities
         /// </summary>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns>A list of entities</returns>
-        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T>?> GetAllAsync(CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Update a entity, handling any business rule before sending the entity to the Repository
+        /// Update a entity in the repository based on it's Guid Id
         /// </summary>
         /// <param name="T">The entity to be updated</param>
         /// <param name="cancellationToken">Cancellation token</param>
@@ -33,7 +34,7 @@
         Task<T> UpdateAsync(T entity, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Delete a entity, handling any business rule before sending the entity to the Repository
+        /// Delete a entity in the repository
         /// </summary>
         /// <param name="id">Entity Id on Guid format</param>
         /// <param name="cancellationToken">Cancellation token</param>
