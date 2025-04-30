80c8f09b2013c79a5c9d8d092e54126092f87ec5
igorthebarros <1igorhbarros@gmail.com>
2025-04-30 01:25:34 -0300

    :sparkles: Add Order DTO and Order Automapper mapping

---
+++ WebAPI/Application/Mapping/OrderMapping.cs
@@ -0,0 +1,26 @@
+﻿using Application.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mapping
+{
+    /// <summary>
+    /// AutoMapper profile for order-related mappings
+    /// </summary>
+    public class OrderMapping : Profile
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrderMappings"/> class
+        /// </summary>
+        public OrderMapping()
+        {
+            CreateMap<Order, OrderDto>()
+                .ForMember(x => x.Status, y => 
+                    y.MapFrom(z => z.Status.ToString()));
+
+            CreateMap<OrderDto, Order>()
+                .ForMember(x => x.Status.ToString(), y =>
+                    y.MapFrom(z => z.Status));
+        }
+    }
+}
