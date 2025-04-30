37c4deb907eff33cb3199954e32b24848d761575
igorthebarros <1igorhbarros@gmail.com>
2025-04-30 01:51:17 -0300

    :sparkles: Add ProductMapping

---
+++ WebAPI/Application/Mapping/ProductMapping.cs
@@ -0,0 +1,21 @@
+﻿using Application.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mapping
+{
+    /// <summary>
+    /// AutoMapper profile for product-related mappings
+    /// </summary>
+    public class ProductMapping : Profile
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrderMappings"/> class
+        /// </summary>
+        public ProductMapping()
+        {
+            CreateMap<Product, ProductDto>();
+            CreateMap<ProductDto, Product>();
+        }
+    }
+}
